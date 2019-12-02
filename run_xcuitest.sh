a58b81190c451c84b9adbd94c12c91fdd42b7a8f
Enrique Gonzalez <enrique@saucelabs.com>
2019-12-02 13:03:44 -0800

    use jq instead of gron to download ipas

--- run_xcuitest.sh
+++ run_xcuitest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 printf "Downloading SimpleLocationApp latest release from Github \n"
 curl -s https://api.github.com/repos/enriquegh/SimpleIOSLocationApp/releases/latest \
-| gron | grep "browser_download_url" \
-| cut -d " " -f 3 | tr -d \; | tr -d \" | xargs -I {} curl -OsL {}
+| jq '.assets[]' | grep "browser_download_url" \
+| cut -d " " -f 4 | tr -d \; | tr -d \" | xargs -I {} curl -OsL {}
 
 printf "Downloading v1.8 of Sauce Labs Runner \n"
 curl -OsL https://s3.amazonaws.com/saucelabs-runner/v1.8/runner.jar
