202b205eaa952970e729c827e7a1ed24761b966d
Enrique Gonzalez <enrique@saucelabs.com>
2019-12-02 12:24:02 -0800

    add print statements to bash script

--- run_xcuitest.sh
+++ run_xcuitest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+printf "Downloading SimpleLocationApp latest release from Github \n"
 curl -s https://api.github.com/repos/enriquegh/SimpleIOSLocationApp/releases/latest \
 | gron | grep "browser_download_url" \
 | cut -d " " -f 3 | tr -d \; | tr -d \" | xargs -I {} curl -OsL {}
 
+printf "Downloading v1.8 of Sauce Labs Runner \n"
 curl -OsL https://s3.amazonaws.com/saucelabs-runner/v1.8/runner.jar
 
+printf "Starting test... \n"
 java -jar runner.jar config \
   --path config.yml \
   --apikey $SAUCE_RDC_API_KEY
