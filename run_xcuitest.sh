11eccad0fe1787fad60f113fdd18f7b72f654800
Enrique Gonzalez <enrique@saucelabs.com>
2020-01-06 09:47:22 -0800

    Revert "hardcode github release"
    This reverts commit e39059ee759c6748787ebe24c2436ea7d33b65e9.

--- run_xcuitest.sh
+++ run_xcuitest.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 printf "Downloading SimpleLocationApp latest release from Github \n"
-# curl -s https://api.github.com/repos/enriquegh/SimpleIOSLocationApp/releases/latest \
-# | jq '.assets[]' | grep "browser_download_url" \
-# | cut -d " " -f 4 | tr -d \; | tr -d \" | xargs -I {} curl -OsL {}
-curl -OsL https://github.com/enriquegh/SimpleIOSLocationApp/releases/download/v1.0.1/SimpleLocationApp.ipa
-curl -OsL https://github.com/enriquegh/SimpleIOSLocationApp/releases/download/v1.0.1/SimpleLocationAppUITests-Runner.ipa
+curl -s https://api.github.com/repos/enriquegh/SimpleIOSLocationApp/releases/latest \
+| jq '.assets[]' | grep "browser_download_url" \
+| cut -d " " -f 4 | tr -d \; | tr -d \" | xargs -I {} curl -OsL {}
 
 printf "Downloading v1.8 of Sauce Labs Runner \n"
 curl -OsL https://s3.amazonaws.com/saucelabs-runner/v1.8/runner.jar
